2ba921612b4e825d92bf85edc42c10797b3d0a7e
Luca Stocchi <lstocchi@redhat.com>
2019-06-28 15:24:06 +0200

    minishift rsp-provider for server connector UI

---
+++ src/extension.ts
@@ -0,0 +1,37 @@
+import * as vscode from 'vscode';
+import * as serverConnectorAPI from 'vscode-server-connector-api';
+import { ExtensionAPI } from './extensionApi';
+import { RSPServer } from 'vscode-server-connector-api/out/util/types';
+import { ServerAPI } from 'vscode-server-connector-api/out/server/serverAPI';
+import { ServerState } from 'vscode-server-connector-api/out/constants';
+import { RSP_PROVIDER_NAME, RSP_PROVIDER_ID } from './constants';
+
+export async function activate(context: vscode.ExtensionContext): Promise<ServerAPI>{
+
+	const api: ExtensionAPI = new ExtensionAPI();
+
+	const rsp: RSPServer = {
+		state: ServerState.UNKNOWN,
+		type: {
+			id: RSP_PROVIDER_ID,
+			visibilename: RSP_PROVIDER_NAME
+		}
+	};
+	const serverConnector = await serverConnectorAPI.extension.RSPProvider.api;
+
+	if (serverConnector.available) {		
+		serverConnector.api.registerRSPProvider(rsp).catch((x: string) =>
+			console.log('error' + x));
+	}
+
+	return api;
+}
+
+// this method is called when your extension is deactivated
+export async function deactivate() {
+	const serverConnector = await serverConnectorAPI.extension.RSPProvider.api;
+
+	if (serverConnector.available) {		
+		serverConnector.api.deregisterRSPProvider(RSP_PROVIDER_ID);
+	}
+}
---
+++ src/extensionApi.ts
@@ -0,0 +1,54 @@
+import * as server from './server';
+import { ServerAPI } from 'vscode-server-connector-api/out/server/serverAPI';
+import { ServerInfo } from 'vscode-server-connector-api/out/util/types';
+import { EventEmitter } from 'events';
+import { ServerState } from 'vscode-server-connector-api/out/constants';
+import { RSP_PROVIDER_NAME } from './constants';
+
+export class ExtensionAPI implements ServerAPI {
+
+    private host: string;
+    private port: number;
+    private emitter: EventEmitter;
+
+    public constructor() {
+        this.host = '';
+        this.port = 0;
+        this.emitter = new EventEmitter();
+    }
+
+    public async startRSP(stdoutCallback: (data: string) => void, stderrCallback: (data: string) => void ): Promise<ServerInfo>  {
+        this.updateRSPStateChanged(ServerState.STARTING);
+        return await server.start(stdoutCallback, stderrCallback).then(serverInfo => {
+            this.host = serverInfo.host;
+            this.port = serverInfo.port;
+            this.updateRSPStateChanged(ServerState.STARTED);
+            return serverInfo;
+        }).catch((error) => {
+            this.updateRSPStateChanged(ServerState.STOPPED);
+            return Promise.reject(`RSP Error - ${RSP_PROVIDER_NAME} failed to start - ${error ? error : ''}`);
+        });
+    }
+
+    public async stopRSP(): Promise<void> {
+        server.terminate().catch(err => { 
+            return err;
+        });
+    }
+
+    public onRSPServerStateChanged(listener: (state: number) => void): void {
+        this.emitter.on('rspServerStateChanged', listener);
+    }
+
+    private async updateRSPStateChanged(state: number): Promise<void> {
+        this.emitter.emit('rspServerStateChanged', state);
+    }
+
+    public getHost(): string {
+        return this.host;
+    }
+
+    public getPort(): number {
+        return this.port;
+    }
+}
